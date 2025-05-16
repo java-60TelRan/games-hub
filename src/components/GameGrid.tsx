8a3dadc0180260904c9758a571c82bb665c0c820
Yuragranovsky <yuragranovsky@gmail.com>
2025-05-16 14:54:42 +0300

    HW #36 solution

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,24 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import api from '../services/api-client'
 import { Game, FetchGamesResponse } from '../model/fetch-game-types'
-import { SimpleGrid} from '@chakra-ui/react'
+import { SimpleGrid, Text} from '@chakra-ui/react'
 import GameCard from './GameCard'
 import { AxiosError } from 'axios'
 
 const GameGrid = () => {
     const [games, setGames] = useState<Game[]>()
+    const [error, setError] = useState<string>("");
     useEffect(() => {
     api.get<FetchGamesResponse>("/games")
     .then(res => setGames(res.data.results))
     .catch((e:AxiosError) => {
-        //TODO
+        setError(e.message)
     }) 
     }, [])
 
     
     
   return (
-    <SimpleGrid columns={{
+    <>
+    {error? <Text color={"red"} fontSize={"2rem"}>{error}</Text> : <SimpleGrid marginStart={{
+      base:8,
+      sm: 5,
+      md:0
+    }}
+    marginEnd={{
+      base: 0,
+      sm: 5,
+      
+    }}
+     columns={{
         base: 1,
         sm: 2,
         md: 3
@@ -26,7 +38,8 @@ const GameGrid = () => {
         {games?.map(g => <GameCard key={g.id} game={g}>
             
             </GameCard>)}
-    </SimpleGrid>
+    </SimpleGrid>}
+    </>
   )
 }
 
--- src/model/fetch-game-types.ts
+++ src/model/fetch-game-types.ts
@@ -1,7 +1,17 @@
+import { NumberInputControlProps } from "@chakra-ui/react";
+
+export interface Platform {
+    platform: {
+        name: number
+    }
+    
+}
 export interface Game {
     id: number;
     name: string;
-    background_image: string
+    background_image: string,
+    metacritic: number,
+    platforms: Platform[]
 }
 export interface FetchGamesResponse {
     count: number;
