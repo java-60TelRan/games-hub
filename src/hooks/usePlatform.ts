633cf125225450fc99cbbc6d7ec9d1047c1ebc12
Yuragranovsky <yuragranovsky@gmail.com>
2025-06-11 17:02:39 +0300

    HW #42 Solution

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,12 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
 import ParentPlatform from "../model/ParentPlatform";
-import apiClient from "../services/api-client";
-import FetchDataResponse from "../model/fetch-data-response";
+import useData from "./useData";
+
 
 export default function usePlatform() {
-     return useQuery<ParentPlatform[], Error>({
-        queryKey: ['platform'],
-        queryFn: () => apiClient.get<FetchDataResponse<ParentPlatform>>("/platforms/lists/parents").then(res => res.data.results),
-        staleTime: 3600 * 1000 * 24
-    })
+     return useData<ParentPlatform>("/platforms/lists/parents");
 }
